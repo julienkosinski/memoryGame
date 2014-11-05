5c2f00650ffa136af30e15c8597472fe92ba1559
julienkosinski <julien.kosinski@gmail.com>
2014-11-05 16:15:02 +0100

    Set up few things

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Instruction to install is coming later.
+To install the appp you have to install nodejs and npm.
+
+Then,
+
+1. Install bower : "npm -g install bower"
+   Install grunt : "npm -g install grunt-cli"
+
+2. Developement mode
+   Run : "grunt serve"
+   
+   Production mode :
+   Run : "grunt build"
--- app/scripts/lib/game.js
+++ app/scripts/lib/game.js
@@ -1,1 +1,31 @@
 'use strict';
+
+var Game = Game || {};
+
+(function(){
+	Game = function(option){
+		var defaultOptions = {
+			nbItem: 5,
+			item: [],
+			inGameId: '#game',
+		}
+		this.option = option || defaultOption;
+		return this;
+	};
+	
+	Game.prototype = {
+		init: function() {
+			var _self = this;
+			for (i=0; i<=_self.option.nbItem; i++) {
+				createItem(i);
+			};
+		},
+		createItem: function(id) {
+			var _self = this;
+
+		},
+		selectItem: function() {
+			
+		}
+    }
+})
