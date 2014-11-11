e2f4168f39fac7c9f8facbceaf842fe3a69a045e
julienkosinski <julien.kosinski@gmail.com>
2014-11-11 20:16:47 +0100

    Fix wrong looping.

--- app/scripts/lib/game.js
+++ app/scripts/lib/game.js
@@ -28,12 +28,10 @@ var gameSession = gameSession || {};
 			var _self = this;
 			if(!_self.buttonClicked) {
 				_self.selectedTile = _self.selectTile();
-				for (var i=0; i<=_self.options.nbTile; i++) {
+				for (var i=0; i<_self.options.nbTile; i++) {
 					_self.createTile(i);
 				}
-				//It does not pass here, why ?
 				_self.buttonClicked = true;
-				console.log(_self.buttonClicked);	
 				
 			}
 			else {
@@ -46,7 +44,44 @@ var gameSession = gameSession || {};
 			$(_self.options.inGameId).append("<div class=\""+_self.options.tileId+"\"><img id=\""+_self.options.tileId+nbId+"\" src=\""+tileData[_self.selectedTile[nbId]].src+"\" alt=\""+tileData[_self.selectedTile[nbId]].name+"\" height=\"116\" width=\"116\"></div>");
 		},
 		toggleDisplay: function() {
+			$(document).on('click', '.tile', function(e){
+				if( !e ) e = window.event;
+				e = e || window.event;
+				var targetedImg = e.srcElement || e.target;
+				//I've chosen to manage an object of active items instead of adding active class in the HTML. That's maybe a bit stupid but it came out of my mind like that for now. If I can't debug it, I'll switch to the other method.
+				if(myGame.activeElements.length < 2) {
+					myGame.activeElements.push({
+						id: targetedImg.id,
+						src: targetedImg.src
+					});
+					//Can't find working expression without condition statment...
+					$("#"+targetedImg.id).fadeTo("slow", $("#"+targetedImg.id).css("opacity") == "0" ? "1" : "1");
+				}
+				//Does not work for the time being but here is my try...
+				else {
+					if(_.each(myGame.activeElements, function (val, i) {
 
+					console.log("i : "+i);
+					console.log("myGame.activeElements[i].src : "+myGame.activeElements[i].src);
+						val.src == myGame.activeElements[i].src;}))
+					{
+						console.log("blo");
+						myGame.tilesFound+=2;
+						if (myGame.tilesFound == nbTile) {
+							alert("You win");
+							myGame.resetGame();
+						}
+					}
+					else {
+						_.each(myGame.activeElements, function () {
+							console.log("bla");
+							$("#"+myGame.activeElements.id[0], "#"+myGame.activeElements.id[1]).fadeTo("slow", $("#"+myGame.activeElements.id[0], "#"+myGame.activeElements.id[1]).css("opacity") == "1" ? "0" : "0");
+							myGame.activeElements = [];
+						})
+					}
+				}
+				myGame.counterClick++;
+			});
 		},
 		selectTile: function() {
 			var _self = this;
